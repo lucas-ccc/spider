0759e7331248fc176479fe63ff320353c5ad34e0
lucas <lucas.ccc@yandex.com>
2022-10-16 00:54:14 +0800

    全部跑通

--- 01-ssr1-scraper-center/main.py
+++ 01-ssr1-scraper-center/main.py
@@ -48,9 +48,9 @@ def parse_detail(html):
     )
     name_pattern = re.compile('<h2.*?>(.*?)</h2>')
     categories_pattern = re.compile(
-        '<button.*?category.*?<highlight-tag.*?>(.*?)</highlight-tag>', re.S
+        '<button.*?category.*?<span>(.*?)</span>.*?</button>', re.S
     )
-    published_at_pattern = re.compile('(\d{4}-\d{2})\s?上映')
+    published_at_pattern = re.compile('(\d{4}-\d{2}-\d{2})\s?上映')
     drama_pattern = re.compile('<div.*?drama.*?>.*?<p.*?>(.*?)</p>', re.S)
     score_pattern = re.compile('<p.*?.score.*?>(.*?)</p>', re.S)
 
@@ -61,8 +61,9 @@ def parse_detail(html):
     categories = re.findall(categories_pattern,
                             html) if re.findall(categories_pattern,
                                                 html) else []
-    published_at = (re.search(published_at_pattern, html).group(1)
-                    ) if re.search(published_at_pattern, html) else None
+    published_at = re.search(published_at_pattern, html).group(1) if re.search(
+        published_at_pattern, html
+    ) else None
     drama = re.search(drama_pattern, html).group(1).strip(
     ) if re.search(drama_pattern, html) else None
     score = float(re.search(score_pattern, html).group(1).strip()
